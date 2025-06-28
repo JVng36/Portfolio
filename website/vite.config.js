b6daccd26c9f5fc7b5dfeb969437bcb6db09c1b0
Jay Vang <jaymac@Mac.attlocal.net>
2025-06-28 16:27:35 -0700

    deploy fixes

--- website/vite.config.js
+++ website/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/', // Replace with your actual repo name
+  base: '/Portfolio/website', // Replace with your actual repo name
   plugins: [react()],
   build: {
     outDir: 'build' // gh-pages expects 'build' folder
